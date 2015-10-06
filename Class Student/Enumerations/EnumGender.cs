61ac40b45b3eff462792dab10e1411ca089b1d41
Peter Velkov <kidroca@gmail.com>
2015-10-06 10:58:06 +0300

    Added XcelStudent for the special assgnment

---
+++ Class Student/Enumerations/EnumGender.cs
@@ -0,0 +1,8 @@
+﻿namespace FunctionalProgramming.Students.Enumerations
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ Class Student/XcelStudent.cs
@@ -0,0 +1,26 @@
+﻿
+namespace FunctionalProgramming.Students
+{
+    using FunctionalProgramming.Students.Enumerations;
+
+    public class XcelStudent : Student
+    {
+        public int ID { get; internal set; }
+
+        public Gender Gender { get; internal set; }
+
+        public StudentType StudentType { get; set; }
+
+        public int ExamResult { get; set; }
+
+        public int HomeworksSent { get; set; }
+
+        public int HomeworksEvaluated { get; set; }
+
+        public double Teamwork { get; set; }
+
+        public double Attendance { get; set; }
+
+        public double Bonus { get; set; }
+    }
+}
